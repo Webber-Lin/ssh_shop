5a2edd752d841f95b04a0d4cbb9f05a4dd28cc62
lzw <798189560@qq.com>
2019-02-08 17:14:25 +0800

    首页查询一级分类商品（左侧分类显示）

--- src/main/java/com/lzw/ssh_shop/category/vo/Category.java
+++ src/main/java/com/lzw/ssh_shop/category/vo/Category.java
@@ -1,11 +1,26 @@
 package com.lzw.ssh_shop.category.vo;
 
+import com.lzw.ssh_shop.categorysecond.vo.CategorySecond;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 一级分类的实体类对象
  */
 public class Category {
     private Integer cid;
     private String cname;
+    //一级分类中存放二级分类的集合:
+    private Set<CategorySecond> categorySeconds=new HashSet<CategorySecond>();
+
+    public Set<CategorySecond> getCategorySeconds() {
+        return categorySeconds;
+    }
+
+    public void setCategorySeconds(Set<CategorySecond> categorySeconds) {
+        this.categorySeconds = categorySeconds;
+    }
 
     public Integer getCid() {
         return cid;
--- src/main/java/com/lzw/ssh_shop/product/action/ProductAction.java
+++ src/main/java/com/lzw/ssh_shop/product/action/ProductAction.java
@@ -1,9 +1,13 @@
 package com.lzw.ssh_shop.product.action;
 
+import com.lzw.ssh_shop.category.service.CategoryService;
+import com.lzw.ssh_shop.category.vo.Category;
 import com.lzw.ssh_shop.product.service.ProductService;
 import com.lzw.ssh_shop.product.vo.Product;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
+
+import java.util.List;
 
 /**
  * 商品的Action对象
@@ -13,6 +17,18 @@ public class ProductAction extends ActionSupport implements ModelDriven<Product>
     private Product product=new Product();
     //注入商品的Service
     private ProductService productService;
+    //接收分类cid
+    private Integer cid;
+    //注入一级分类的Service
+    private CategoryService categoryService;
+
+    public void setCategoryService(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
 
     public void setProductService(ProductService productService) {
         this.productService = productService;
@@ -27,5 +43,11 @@ public class ProductAction extends ActionSupport implements ModelDriven<Product>
         //调用Service的方法完成查询
         product=productService.findByPid(product.getPid());
         return "findByPid";
+    }
+
+    //根据分类的id查询商品
+    public String findByCid(){
+        // List<Category> cList=categoryService.findAll();
+        return "findByCid";
     }
 }
